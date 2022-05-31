46ad72b5cd2efa229d1e4fe5c26bba4635a3d3bb
tamara <tamara@ubuntu20.04>
2022-05-31 16:59:56 +0300

    Minor changes

--- games/accounts/views.py
+++ games/accounts/views.py
@@ -4,7 +4,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Account
+from django.views import View
 
+class TestView(View):
+    def get(self, request):   
+        return render(request, 'accounts/base.html', {})
 
 # Create your views here.
 class AccountCreate(CreateView):
--- games/games/urls.py
+++ games/games/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import TestView
 
 urlpatterns = [
+    path('', TestView.as_view(), name='test'),
     path('admin/', admin.site.urls),
 ]
