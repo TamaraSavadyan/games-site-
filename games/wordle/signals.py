1347c8209768f009966470edc61c34f93bde5c97
TamaraSavadyan <savadyan.tamara@gmail.com>
2022-07-26 13:58:20 +0300

    changes..

--- games/wordle/signals.py
+++ games/wordle/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_save
 from .models import Wordle
 from accounts.models import Account
 
+
 @receiver(post_save, sender=Account)
 def create_account_wordle(sender, instance, **kwargs):
     Wordle.objects.create(account=instance).save()
-
